6a1856123d5480b92428f46e36af6645bf56850a
Sebastinas-Dev <developer.sebastians@gmail.com>
2024-02-27 15:33:07 -0500

    second version-1

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   const [imageSelected, SetImageSelected] = useState(initialImage)
 
   const objStyle = {
-    backgroundImage: `url("/src/assets/img/fondo${imageSelected}.jpg")`,
+    backgroundImage: `url("/public/img/fondo${imageSelected}.jpg")`,
   }
   return (
     <div className='app' style={objStyle}>
